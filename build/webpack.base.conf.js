6596010958707a1361b613036080ad4657cd68f2
lishan <lis@drore.com>
2018-04-13 10:00:00 +0800

    集成jquery

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -29,7 +30,14 @@ module.exports = {
       '@': resolve('src'),
     }
   },
-  module: {
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+
+    module: {
     rules: [
       {
         test: /\.vue$/,
