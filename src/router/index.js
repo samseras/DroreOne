5692513302e0babeb27093133ff4d4256274aa72
xuyx <xuyx@drore.com>
2018-05-22 16:01:20 +0800

    告警规则

--- src/router/index.js
+++ src/router/index.js
@@ -112,7 +112,12 @@ import PersonType from '@/components/personChildren/personType'
 import Alarm from '@/components/alarm'
 import alarmcolumnRule from '@/components/eye/alarm/alarmRule/alarmcolumnRule'
 import warningEvent from '@/components/eye/alarm/alarmEvent/warningEvent'
-
+import firefightingRule from '@/components/eye/alarm/alarmRule/firefightingRule'
+import crossborderRule from  '@/components/eye/alarm/alarmRule/crossborderRule'
+import offtrackRule from '@/components/eye/alarm/alarmRule/offtrackRule'
+import overlimitRule from '@/components/eye/alarm/alarmRule/overlimitRule'
+import waterlevelRule from  '@/components/eye/alarm/alarmRule/waterlevelRule'
+import conditionRule from '@/components/eye/alarm/alarmRule/conditionRule'
 // 数据分析（图表）
 import Analyze from '@/components/analyze'
 import Passenger from '@/components/analysisSystem/analyze/passengerFlow'
@@ -218,13 +223,13 @@ const routes = [
                 children:[
 
                     {path:'/alarm/alarmcolumn-Rule',name:'alarmcolumnRule',component:alarmcolumnRule},
-                    // {path:'/alarm/firefighting-Rule',name:'firefightingRule',component:firefightingRule},
-                    // {path:'/alarm/crossborder-Rule',name:'crossborderRule',component:crossborderRule},
+                    {path:'/alarm/firefighting-Rule',name:'firefightingRule',component:firefightingRule},
+                    {path:'/alarm/crossborder-Rule',name:'crossborderRule',component:crossborderRule},
                     // {path:'/alarm/speeding-Rule',name:'speendingRule',component:speendingRule},
-                    // {path:'/alarm/offtrack-Rule',name:'offtrackRule',component:offtrackRule},
-                    // {path:'/alarm/overlimit-Rule',name:'overlimitRule',component:overlimitRule},
-                    // {path:'/alarm/waterlevel-Rule',name:'waterlevelRule',component:waterlevelRule},
-                    // {path:'/alarm/condition-Rule',name:'conditionRule',component:conditionRule},
+                    {path:'/alarm/offtrack-Rule',name:'offtrackRule',component:offtrackRule},
+                    {path:'/alarm/overlimit-Rule',name:'overlimitRule',component:overlimitRule},
+                    {path:'/alarm/waterlevel-Rule',name:'waterlevelRule',component:waterlevelRule},
+                    {path:'/alarm/condition-Rule',name:'conditionRule',component:conditionRule},
                     {path:'/alarm/warning-Event',name:'warningEvent',component:warningEvent}
                 ]
             }
