0d6b1a333a5be1694beed2695407f1e23cfac129
liuyu <you@example.com>
2018-05-29 15:32:21 +0800

    修改卡片描述隐藏

--- src/components/propertyChildren/hardwareDeploy/lampLightHardware.vue
+++ src/components/propertyChildren/hardwareDeploy/lampLightHardware.vue
@@ -52,8 +52,7 @@
                             </template>
 
                         </el-table-column>
-
-
+                        
                         <el-table-column
                             prop="serialNum"
                             label="路灯型号">
@@ -64,8 +63,16 @@
                             label="所属片区">
                         </el-table-column>
                         <el-table-column
-                            prop="description"
                             label="描述">
+                            <template slot-scope="scope">
+                                <div class="box" v-if="scope.row.description">
+                                    <div class="bottom">
+                                        <el-tooltip class="item" effect="light" :content=scope.row.description placement="bottom">
+                                            <el-button>{{scope.row.description}}</el-button>
+                                        </el-tooltip>
+                                    </div>
+                                </div>
+                            </template>
                         </el-table-column>
                         <el-table-column>
                             <template slot-scope="scope">
@@ -462,6 +469,17 @@
         }
         .el-checkbox__inner{
             margin-top:rem(5);
+        }
+        .cameraList .el-button{
+            border:1px solid transparent;
+            text-align: left;
+        }
+        .cameraList .box .el-button span{
+            display:inline-block;
+            width: 150px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
         }
     }
 </style>
