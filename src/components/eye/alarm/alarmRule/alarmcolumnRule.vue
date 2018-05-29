3981a240528ab09daa5c44a85e19cc04711a4177
xuyx <xuyx@drore.com>
2018-05-29 14:47:46 +0800

    修改告警

--- src/components/eye/alarm/alarmRule/alarmcolumnRule.vue
+++ src/components/eye/alarm/alarmRule/alarmcolumnRule.vue
@@ -270,7 +270,6 @@
                     return
                 }
 
-
                 if ( this.choseInfoId.length > 0) {
                     this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
                         confirmButtonText: '确定',
