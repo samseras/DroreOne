0395806639e0baa3c7d7cf6320c6ce159931a256
Liumx <liumx@drore.com>
2018-06-04 19:04:03 +0800

    修改登录用户名

--- src/components/pages/login.vue
+++ src/components/pages/login.vue
@@ -85,6 +85,7 @@
 				if(this.checkCode !== "") {
                     let obj = `BASIC ${Base64.encode(this.username +  ":"+ this.password)}`
                     await api.login.userLogin(obj).then(res => {
+                        localStorage.setItem('userName',this.username)
                         localStorage.setItem('token', JSON.stringify(obj))
                         this.$store.commit('SET_USER', this.username)
                         if(res != []){
